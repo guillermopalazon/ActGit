22f33bdf4d4c9c0324e90e6a46b8bd2fef54854c
Guillermo Palazón <guillermo.palazon@murciaeduca.es>
2023-05-26 12:18:52 +0200

    Versión inicial del proyecto

---
+++ src/main/java/com/guillermopalazon/actgit/ActGit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.guillermopalazon.actgit;
+
+/**
+ *
+ * @author Guillermo
+ */
+public class ActGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
